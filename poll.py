4c07db9665d46dadd97ca4a3e5380b1dd72dc461
Tim Robles <timrobles@gmail.com>
2009-11-04 21:41:26 -0800

    added crossdomain

--- poll.py
+++ poll.py
@@ -4,7 +4,6 @@ import datetime
 import logging
 import re
 import simplejson
-import time
 import urllib
 
 import twython
@@ -20,13 +19,15 @@ class TweetUser(db.Model):
   uid = db.IntegerProperty()
   location = db.StringProperty()
   geo = db.GeoPtProperty()
+  name = db.StringProperty()
+  profile_image = db.StringProperty()
 
 class Tweet(db.Model):
   user = db.ReferenceProperty(TweetUser, collection_name="tweets")
   id = db.IntegerProperty()
   uid = db.IntegerProperty()
   text = db.StringProperty(multiline=True)
-  time = db.TimeProperty()
+  time = db.DateTimeProperty()
 
 class PollHandler(webapp.RequestHandler):
   
@@ -34,19 +35,17 @@ class PollHandler(webapp.RequestHandler):
     self.twitter = twython.setup()
     
   def get(self):
+    
+    # flush the data here to force a refesh
     flush = self.request.get("flush")
     if flush == "true":
       self.flush_all()
-    # self.flush_all()
+    
     results = self.get_search("underthehood")
     self.update_twitter_locations()
     self.update_geocoded_locations()
-    # only get users with geo information
-    result = db.GqlQuery("SELECT * FROM Tweet");
-    logging.info("number of tweets: %s" % results.count())
-    # for tweet in results:
-    #       logging.info("tweet user: %s" % tweet.user)
     
+    result = db.GqlQuery("SELECT * FROM Tweet");
     self.response.headers['Content-Type'] = 'application/json'
     self.response.out.write(GqlEncoder().encode(result))
   
@@ -69,6 +68,7 @@ class PollHandler(webapp.RequestHandler):
   def query_geo_location(self, tweet_user):
     
     # prepare the location, search for coordinates
+    # usually looks something like iPhone: -30.0,-40.0
     m = re.findall("-?\d+.\d+,\-?\d+.\d+", tweet_user.location)
     
     if len(m) > 0:
@@ -107,10 +107,6 @@ class PollHandler(webapp.RequestHandler):
       if tweet_user.location is None:
         self.query_location(tweet_user)
 
-    # tweet_users2 = db.GqlQuery("SELECT * FROM TweetUser")
-    #     for tweet_user2 in tweet_users2:
-    #       self.response.out.write(" >> user id: " + str(tweet_user2.uid) + ", location: " + str(tweet_user2.location)  + "   |    ")
-    
   def query_location(self, tweet_user):
     mem_key = str(tweet_user.ref_id)
     location = memcache.get(mem_key)
@@ -147,42 +143,74 @@ class PollHandler(webapp.RequestHandler):
       
   """ does the actual API query"""
   def query_search(self, terms):
-    tweets = db.GqlQuery("SELECT * FROM Tweet")
+    
     try:
       search_results = self.twitter.searchTwitter(terms, rpp="100")
     except:
       logging.error("searchTwitter API call failed")
-    try:
-      for tweet in search_results["results"]:
-        tweet_id = tweet["id"];
-        tweet_uid = tweet["from_user_id"]
-        tweet_text = tweet["text"].replace('"', '\"')
-        # if this is a new user, add it to our TweetUser table
-        db_tweet_user = db.GqlQuery("SELECT * FROM TweetUser WHERE uid = :1", tweet_uid)
-        if db_tweet_user.count() == 0:
-          tweet_user = TweetUser(ref_id=tweet_id,
-                                 uid=tweet_uid,
-                                 location=None,
-                                 geo=None)
-          db.put(tweet_user)
-        
-        result = db.GqlQuery("SELECT * FROM TweetUser WHERE uid = :1", tweet_uid)
-        tweet_user = result[0]
-          
-        # dont add duplicate tweets
-        db_tweet = db.GqlQuery("SELECT * FROM Tweet WHERE id = :1", tweet_id)
-        if db_tweet.count() == 0:
-          tweet = Tweet(user=tweet_user,
-                        id=tweet_id,
-                        uid=tweet_uid,
-                        text=tweet_text,
-                        time=None)
-          db.put(tweet)
-    except Exception:
-      logging.error("an error has occured with query_search")
+      return db.GqlQuery("SELECT * FROM Tweet")
+      
+    logging.info("number of results from search api: %s" % len(search_results["results"]))
+    
+    for tweet in search_results["results"]:
+
+      # add or update our user table with this new tweet  
+      tweet_user = self.add_or_update_user(tweet)
+      
+      self.add_or_update_tweet(tweet, tweet_user)
                          
     return db.GqlQuery("SELECT * FROM Tweet")
   
+  """ using add or update since we might need to add or update fields if the db model has changed """
+  def add_or_update_user(self, tweet_from_search):
+    
+    tweet_id = tweet_from_search["id"];
+    tweet_uid = tweet_from_search["from_user_id"]
+    tweet_name = tweet_from_search["from_user"]
+    tweet_profile_image = tweet_from_search["profile_image_url"]
+    
+    # if this is a new user, add it to our TweetUser table
+    db_tweet_user = db.GqlQuery("SELECT * FROM TweetUser WHERE uid = :1", tweet_uid)
+    
+    if db_tweet_user.count() == 0:
+      tweet_user = TweetUser()
+    else:
+      tweet_user = db_tweet_user[0]
+    
+    # a db.put call could probably be saved if we check that the new values are different
+    tweet_user.ref_id = tweet_id
+    tweet_user.uid = tweet_uid
+    tweet_user.name = tweet_name
+    tweet_user.profile_image = tweet_profile_image
+    
+    db.put(tweet_user)
+          
+    return tweet_user
+
+  """ adding time values to the tweet, so using an update method """
+  def add_or_update_tweet(self, tweet_from_search, tweet_user):
+    
+    tweet_id = tweet_from_search["id"];
+    tweet_uid = tweet_from_search["from_user_id"]
+    # replace quotes with escaped versions to not break json
+    tweet_text = tweet_from_search["text"].replace('"', '\"')
+    tweet_time = datetime.datetime.strptime(tweet_from_search["created_at"], '%a, %d %b %Y %H:%M:%S +0000')
+    
+    db_tweet = db.GqlQuery("SELECT * FROM Tweet WHERE id = :1", tweet_id)
+    
+    if db_tweet.count() == 0:
+      tweet = Tweet()
+    else:
+      tweet = db_tweet[0]
+    
+    tweet.user = tweet_user
+    tweet.id = tweet_id
+    tweet.uid = tweet_uid
+    tweet.text = tweet_text
+    tweet.time = tweet_time
+    
+    db.put(tweet)                
+    
   def clear_tweet(self):
     q = db.GqlQuery("SELECT * FROM Tweet")
     if q.count() != 0:
